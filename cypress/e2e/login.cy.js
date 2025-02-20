f6b1e74e3a79df80decf554a8ca91cd969752b83
Rafael Bizarria <111367244+rafaelbpmt@users.noreply.github.com>
2025-02-20 16:17:35 -0300

    base url added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 import userData from '../fixtures/user-data.json'
 
->>>>>>> cd88710 (Added userdata)
 describe('Orange HRM Tests', () => {
 
 /* objeto */
@@ -11,39 +8,22 @@ const selectorsList = {
   passwordField: "[name='password']",
   loginButton: "[type='submit']",
   sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-<<<<<<< HEAD
-=======
-  dashboardGrid: ".orangehrm-dashboard-grid",
->>>>>>> cd88710 (Added userdata)
   wrongCredentialAlert: "[role='alert']"
 } 
 
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-<<<<<<< HEAD
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
-=======
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
->>>>>>> cd88710 (Added userdata)
+    cy.get(selectorsList.sectionTitleTopBar)
   })
 
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-<<<<<<< HEAD
-    cy.get(selectorsList.usernameField).type('Admin1')
-    cy.get(selectorsList.passwordField).type('admin123')
-=======
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
->>>>>>> cd88710 (Added userdata)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)  
   })
